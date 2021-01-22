9b65d6cbbb2905a146b0cd9787893aa140de2dbf
Clucas0311 <clucas0311@gmail.com>
2021-01-22 15:59:58 -0500

    Adding router

--- routes/wiki.js
+++ routes/wiki.js
@@ -1,16 +1,17 @@
 const express = require('express');
+const { addPage } = require('../views');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-  res.send('get success');
+	res.send('get success');
 });
 
 router.post('/', (req, res) => {
-  res.send('post success');
+	res.send('post success');
 });
 
 router.get('/add', (req, res) => {
-  res.send('get/add success');
+	res.send(addPage());
 });
 
 module.exports = router;
